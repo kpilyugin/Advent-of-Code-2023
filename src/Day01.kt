59aa77232b0df761da4b8a9e342c3dc12a42f6e8
Kirill Pilyugin <kirill.pilyugin@delightex.com>
2023-12-01 14:14:24 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            line -> 10 * line.first { it.isDigit() }.digitToInt() + line.last { it.isDigit() }.digitToInt()
+        }
     }
 
+    val digits = mutableMapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
+    for (digit in 1..9) {
+        digits[digit.toString()] = digit
+    }
     fun part2(input: List<String>): Int {
-        return input.size
+        fun Pair<Int, String>?.toDigit(): Int = digits[this!!.second]!!
+        return input.sumOf {
+            line -> 10 * line.findAnyOf(digits.keys).toDigit() + line.findLastAnyOf(digits.keys).toDigit()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+    val testInput2 = readInput("Day01_test2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
